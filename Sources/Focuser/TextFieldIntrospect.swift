cbec891cf2f27fe7f650c23792025202e2d4af5b
GST <gst.soft.65536@gmail.com>
2023-04-17 21:00:24 +0900

    Update: Remove Redundant UIDelgate Methods Overriding

--- Sources/Focuser/TextFieldIntrospect.swift
+++ Sources/Focuser/TextFieldIntrospect.swift
@@ -13,46 +13,6 @@ class TextFieldObserver: NSObject, UITextFieldDelegate {
     weak var forwardToDelegate: UITextFieldDelegate?
     
     @available(iOS 2.0, *)
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        forwardToDelegate?.textFieldShouldBeginEditing?(textField) ?? true
-    }
-
-    @available(iOS 2.0, *)
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        forwardToDelegate?.textFieldDidBeginEditing?(textField)
-    }
-
-    @available(iOS 2.0, *)
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        forwardToDelegate?.textFieldShouldEndEditing?(textField) ?? true
-    }
-
-    @available(iOS 2.0, *)
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        forwardToDelegate?.textFieldDidEndEditing?(textField)
-    }
-
-    @available(iOS 10.0, *)
-    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
-        forwardToDelegate?.textFieldDidEndEditing?(textField)
-    }
-
-    @available(iOS 2.0, *)
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        forwardToDelegate?.textField?(textField, shouldChangeCharactersIn: range, replacementString: string) ?? true
-    }
-    
-    @available(iOS 13.0, *)
-    func textFieldDidChangeSelection(_ textField: UITextField) {
-        forwardToDelegate?.textFieldDidChangeSelection?(textField)
-    }
-
-    @available(iOS 2.0, *)
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        forwardToDelegate?.textFieldShouldClear?(textField) ?? true
-    }
-    
-    @available(iOS 2.0, *)
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         onReturnTap()
         return forwardToDelegate?.textFieldShouldReturn?(textField) ?? true
